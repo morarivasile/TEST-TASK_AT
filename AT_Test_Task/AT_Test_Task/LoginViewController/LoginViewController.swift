a68316a56f92b3bfddcf13ccd0207a710e8630c9
Vasile Morari <vasile.morari@icloud.com>
2020-09-24 17:19:11 +0300

    Handled keyboard.

--- AT_Test_Task/AT_Test_Task/LoginViewController/LoginViewController.swift
+++ AT_Test_Task/AT_Test_Task/LoginViewController/LoginViewController.swift
@@ -23,6 +23,8 @@ final class LoginViewController: UIViewController {
     
     @IBOutlet weak private var loginButton: UIButton!
     
+    @IBOutlet weak private var centerYAnchor: NSLayoutConstraint!
+    
     // MARK: - Public properties
     
     var interactor: LoginInteractorProtocol!
@@ -31,6 +33,12 @@ final class LoginViewController: UIViewController {
         super.viewDidLoad()
         
         title = "Авторизация"
+        startObserving()
+        addGestures()
+    }
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
     }
     
     // MARK: - IBActions
@@ -38,7 +46,47 @@ final class LoginViewController: UIViewController {
     @IBAction private func didTapLoginButton(_ sender: UIButton) {
         interactor.didTapLoginButton()
     }
+}
+
+// MARK: - Private (keyboard)
+
+extension LoginViewController {
+    private func addGestures() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard)))
+    }
     
+    private func startObserving() {
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustInsetForKeyboard(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustInsetForKeyboard(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func adjustInsetForKeyboard(_ notification: NSNotification) {
+        guard let userInfo = notification.userInfo else { return }
+        
+        guard let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
+        guard let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? Int else { return }
+        
+        guard let endFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] else { return }
+        guard let keyboardFrame = (endFrame as? NSValue)?.cgRectValue else { return }
+        
+        let show = (notification.name == UIResponder.keyboardWillShowNotification) ? true : false
+        let keyboardPadding: CGFloat = 10.0
+        
+        UIView.animate(withDuration: TimeInterval(duration), delay: 0, options: [UIView.AnimationOptions(rawValue: UInt(curve))], animations: {
+            if show {
+                let bottomLoginButtonPoint = self.view.frame.height - self.loginButton.frame.origin.y - self.loginButton.frame.height
+                if keyboardFrame.height + keyboardPadding > bottomLoginButtonPoint {
+                    let difference = keyboardFrame.height - bottomLoginButtonPoint
+                    self.centerYAnchor.constant -= difference + keyboardPadding
+                }
+            } else {
+                self.centerYAnchor.constant = 0.0
+            }
+            
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        
+    }
 }
 
 // MARK: - LoginViewProtocol
