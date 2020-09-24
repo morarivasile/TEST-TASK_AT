09c2f35dc5b1c30a92ba143f7cf783db8576ffc7
Vasile Morari <vasile.morari@icloud.com>
2020-09-24 11:01:33 +0300

    Added initial ies template project

---
+++ AT_Test_Task/AT_Test_Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AT_Test_Task
+//
+//  Created by Vasile Morari on 24.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
