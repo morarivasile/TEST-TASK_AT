7fcdf310350b22be21731d06c3168d29a36f37a1
Vasile Morari <vasile.morari@icloud.com>
2020-09-24 13:58:43 +0300

    Text translated to russian.

--- AT_Test_Task/AT_Test_Task/LoginViewController/LoginViewController.swift
+++ AT_Test_Task/AT_Test_Task/LoginViewController/LoginViewController.swift
@@ -28,7 +28,7 @@ final class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "Authorization"
+        title = "Авторизация"
     }
     
     // MARK: - IBActions
